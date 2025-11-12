c23fe66bf2bd620e149a2a1c5bc68330265860f1
Aagam <aagam.shah@beatoapp.com>
2025-11-12 12:49:05 +0530

    first commit

---
+++ app/src/main/java/com/aagamshah/personalspace/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.aagamshah.personalspace
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.aagamshah.personalspace.ui.theme.PersonalSpaceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PersonalSpaceTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PersonalSpaceTheme {
+        Greeting("Android")
+    }
+}
+//
+//git remote add origin https://github.com/Aagam9833/PersonalSpace.git
+//git branch -M master
+//git push -u origin master
